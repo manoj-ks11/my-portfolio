252303f7de0d4be161724c14abea6d09533fde3c
manoj-ks11 <manoj.senapaty97@gmail.com>
2025-08-14 08:48:48 +0530

    initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+
 import { cn } from "@/lib/utils";
 import { Menu, X } from "lucide-react";
 import { useEffect, useState } from "react";
@@ -65,19 +66,19 @@ export const Navbar = () => {
 
         <div
           className={cn(
-            "fixed inset-0 bg-background/95 backdroup-blur-md z-40 flex flex-col items-center justify-center",
+            "fixed inset-0 bg-background/95 backdroup-blur-md z-40 flex flex-col items-center justify-center ",
             "transition-all duration-300 md:hidden",
             isMenuOpen
               ? "opacity-100 pointer-events-auto"
               : "opacity-0 pointer-events-none"
           )}
         >
-          <div className="flex flex-col space-y-8 text-xl">
+          <div className="flex flex-col space-y-8 text-xl ">
             {navItems.map((item, key) => (
               <a
                 key={key}
                 href={item.href}
-                className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                className="text-foreground/80 hover:text-primary transition-colors duration-300 "
                 onClick={() => setIsMenuOpen(false)}
               >
                 {item.name}
@@ -88,4 +89,4 @@ export const Navbar = () => {
       </div>
     </nav>
   );
-};
+}; 
